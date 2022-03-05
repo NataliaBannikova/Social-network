c60b8d17a38ca352e2ee99620a14b1f5014d666b
NB <natabannikova10@gmale.com>
2022-03-05 18:08:54 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.db6196f425c74aafb328b5cccd8a5079.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c9832201b143f9bbe05223ea92d494",
+    "url": "/Social-network/index.html"
+  },
+  {
+    "revision": "79808cde2bef5281232c",
+    "url": "/Social-network/static/css/4.747ebca0.chunk.css"
+  },
+  {
+    "revision": "81bda14afd1754f156f9",
+    "url": "/Social-network/static/css/5.58e0e6d1.chunk.css"
+  },
+  {
+    "revision": "9de23ee2f60376f4677b",
+    "url": "/Social-network/static/css/main.0ab15d16.chunk.css"
+  },
+  {
+    "revision": "ec530cecf5119d161f2b",
+    "url": "/Social-network/static/js/2.e4358bac.chunk.js"
+  },
+  {
+    "revision": "88487546edac862af49cea1b71b5539f",
+    "url": "/Social-network/static/js/2.e4358bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083de8be177d514cf98d",
+    "url": "/Social-network/static/js/3.431484f9.chunk.js"
+  },
+  {
+    "revision": "b78d06d995050d23ec508cb4ba84482c",
+    "url": "/Social-network/static/js/3.431484f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79808cde2bef5281232c",
+    "url": "/Social-network/static/js/4.37165afe.chunk.js"
+  },
+  {
+    "revision": "81bda14afd1754f156f9",
+    "url": "/Social-network/static/js/5.fabf5ef8.chunk.js"
+  },
+  {
+    "revision": "f12e886894f82443a0ba",
+    "url": "/Social-network/static/js/6.d868440b.chunk.js"
+  },
+  {
+    "revision": "9de23ee2f60376f4677b",
+    "url": "/Social-network/static/js/main.c3a82044.chunk.js"
+  },
+  {
+    "revision": "8e11959db965b64e95c1",
+    "url": "/Social-network/static/js/runtime-main.04d8bdab.js"
+  },
+  {
+    "revision": "d2253ba222264c7576d8b56191bb6121",
+    "url": "/Social-network/static/media/loader.d2253ba2.svg"
+  },
+  {
+    "revision": "0aa8581c2cb0aa948d63ce3ddad90c81",
+    "url": "/Social-network/static/media/user.0aa8581c.jpeg"
+  }
+]);
